85058994a1f0cfce8f3b8577b1a3e672d19234f5
Thomas COLLIN <knutelator@hotmail.fr>
2020-08-08 09:58:12 +0200

    Rajout sécurité

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const sauceRoutes= require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
+require('dotenv').config()
+
 //Code pour la connexion à la base de données
-mongoose.connect('mongodb+srv://Admin:Openclassroom@cluster0.miaiw.mongodb.net/So_Pekocko?retryWrites=true&w=majority',//LectureContenu:OpenclassroomLecture
+mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0.miaiw.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority',//LectureContenu:OpenclassroomLecture
 
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -15,6 +18,7 @@ mongoose.connect('mongodb+srv://Admin:Openclassroom@cluster0.miaiw.mongodb.net/S
 
 // Mise en place de la const app
 const app = express();
+app.use(helmet());
 //Mise en place du CORS pour éviter les erreurs en cas de serveurs différents
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,7 +26,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-//code permettant de récupérer le contenu de la requète
+
+  //code permettant de récupérer le contenu de la requète
 app.use(bodyParser.json());
 
 //Code pour indiquer l'emplacement de stockage des images
